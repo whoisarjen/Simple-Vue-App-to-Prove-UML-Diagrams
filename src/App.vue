ca6f42c1c19d5523ac10547cbf146a76f2319d60
whoisarjen <kamilow97@gmail.com>
2021-12-13 15:26:24 +0100

    v

--- src/App.vue
+++ src/App.vue
@@ -3,19 +3,43 @@
     <v-app-bar
       app
     >
-      <v-spacer></v-spacer>
-      <router-link to="/">Check history</router-link>
-      <v-spacer></v-spacer>
-      <router-link to="/deposit">Deposit</router-link>
-      <v-spacer></v-spacer>
-      <router-link to="/withdraw">Withdraw</router-link>
-      <v-spacer></v-spacer>
-      <router-link to="/loan">Loan</router-link>
-      <v-spacer></v-spacer>
-      <router-link to="/transfer">Transfer</router-link>
-      <v-spacer></v-spacer>
-      <router-link to="/login">Change client</router-link>
-      <v-spacer></v-spacer>
+      <v-spacer class="notMobile"></v-spacer>
+      <router-link class="notMobile" to="/">Check history</router-link>
+      <v-spacer class="notMobile"></v-spacer>
+      <router-link class="notMobile" to="/deposit">Deposit</router-link>
+      <v-spacer class="notMobile"></v-spacer>
+      <router-link class="notMobile" to="/withdraw">Withdraw</router-link>
+      <v-spacer class="notMobile"></v-spacer>
+      <router-link class="notMobile" to="/loan">Loan</router-link>
+      <v-spacer class="notMobile"></v-spacer>
+      <router-link class="notMobile" to="/transfer">Transfer</router-link>
+      <v-spacer class="notMobile"></v-spacer>
+      <router-link class="notMobile" to="/login">Change client</router-link>
+      <v-spacer class="notMobile"></v-spacer>
+      <v-menu
+        bottom
+        left
+      >
+        <template v-slot:activator="{ on, attrs }">
+          <v-btn
+            icon
+            color="primary"
+            v-bind="attrs"
+            v-on="on"
+          >
+            <v-icon>mdi-dots-vertical</v-icon>
+          </v-btn>
+        </template>
+
+        <v-list>
+          <v-list-item
+            v-for="(item, i) in items"
+            :key="i"
+          >
+            <router-link :to="item.location"><v-list-item-title>{{ item.title }}</v-list-item-title></router-link>
+          </v-list-item>
+        </v-list>
+      </v-menu>
     </v-app-bar>
 
     <v-main>
@@ -29,7 +53,14 @@
 export default {
   name: 'App',
   data: () => ({
-    //
+    items: [
+      { title: 'Check history', location: '/' },
+      { title: 'Deposit', location: '/deposit' },
+      { title: 'Withdraw', location: '/withdraw' },
+      { title: 'Loan', location: '/loan' },
+      { title: 'Transfer', location: '/transfer' },
+      { title: 'Change client', location: '/login' }
+    ]
   }),
   watch: {
     $route () {
@@ -40,3 +71,11 @@ export default {
   }
 }
 </script>
+
+<style>
+  @media (max-width: 600px){
+    .notMobile{
+      display: none;
+    }
+  }
+</style>
